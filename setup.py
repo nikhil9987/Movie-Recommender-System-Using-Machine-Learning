80f811b2c0ee37b63c6ea98e1f84df2724a40b30
nikhil9987 <73032701+nikhil9987@users.noreply.github.com>
2024-11-01 15:34:48 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
+AUTHOR_USER_NAME = "nikhil9987"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="sawant.nikhil724@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.7",
+)
